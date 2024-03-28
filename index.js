1e4cd8173d0ed20988fa100450f22e08364e01a2
Mekhi-M <mekhim0731@gmail.com>
2024-03-28 14:25:05 -0400

    Question 2 done

--- index.js
+++ index.js
@@ -8,4 +8,17 @@ const fiveToOneHundred = () => {
     return;
 }
 
-fiveToOneHundred();
+//fiveToOneHundred();
+
+//Answer to Q2
+const multiplesOfThree = () => {
+
+    for (let i = 1; i < 101; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+    return;
+}
+
+multiplesOfThree();
